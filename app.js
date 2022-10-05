8ccb8f1dfb8f05598ad334850ce3514e0d44508d
nakhileship <www.nakhileshnakhu@gmail.com>
2022-10-05 23:34:56 +0530

    update

---
+++ app.js
@@ -0,0 +1,33 @@
+// listen for submit
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+// calculate Result
+function calculateResults(e) {
+    console.log('Calculating...');
+    // UI Vars
+    const amount = document.getElementById('amount');
+    const interst = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const amtotalInterest = document.getElementById('total-interest');
+
+    const principal = parseFloat(amount.value);
+    const calculatedInterst = parseFloat(interst.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+    // compute monthly payment
+    const x = Math.pow(1 = calculatedInterst, calculatedPayments);
+    const monthly = (principal * x * calculatedInterst) / (x - 1);
+
+    if (isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principl).toFixed(2);
+
+    } else {
+
+    }
+
+
+    e.preventDefault();
+}
